3b8467afff2ede11588817ed9fb3bcde1a1d11d3
Jose Juarez Junior <josejuarez.junior@ziliatech.com>
2024-08-28 16:18:16 -0300

    feat: Adding DbContext and Creating model classes

---
+++ UserManagement.Infra.Data/EntitiesConfiguration/UserConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using UserManagement.Domain.Entities;
+
+namespace UserManagement.Infra.Data.EntitiesConfiguration
+{
+    public class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Name).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.BirthDate).IsRequired();
+            builder.Property(x => x.Email).IsRequired();
+        }
+    }
+}
---
+++ UserManagement.Infra.Data/Repositories/UserRepository.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UserManagement.Domain.Entities;
+using UserManagement.Domain.Interfaces;
+using UserManagement.Infra.Data.Context;
+
+namespace UserManagement.Infra.Data.Repositories
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly ApplicationDbContext _context;
+
+        public UserRepository(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IEnumerable<User>> GetUsersAsync()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        public async Task<User> GetUserByIdAsync(int? id)
+        {
+            return await _context.Users.FindAsync(id);
+        }
+
+        public async Task<User> CreateUserAsync(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> UpdateUserAsync(User user)
+        {
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+            return user;
+        }
+
+        public async Task<User> DeleteUserAsync(User user)
+        {
+            _context.Remove(user);
+            await _context.SaveChangesAsync();
+            return user;
+        }
+    }
+}
